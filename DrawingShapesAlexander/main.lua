fdc6b8f3a68a1b6e991cf925344de26d795995e1
Alexander Matheson <Alexander.Matheson@ocsbstudent.ca>
2020-03-02 14:30:06 -0500

    added moving images

--- DrawingShapesAlexander/main.lua
+++ DrawingShapesAlexander/main.lua
@@ -6,5 +6,17 @@
 --Hide status bar
 display.setStatusBar(display.HiddenStatusBar)
 --set variables
-local verticiesTri = {256, 10, 502, 364, 10, 364}
-display.newPolygon( display.contentHeight/4, display.contentWidth/4, verticiesTri)
+local verticiesTri = {306, 50, 502, 364, 120, 364}
+local verticiestPen = {708, 50, 828, 50, 858, 192, 768, 364, 678, 192}
+local verticiesHex= {206, 404, 306, 404, 356, 454, 306, 504, 206, 504, 156, 454}
+
+--Display Triangle
+display.newPolygon( display.contentHeight/4+100, display.contentWidth/4-50, verticiesTri)
+
+--Display Pentagon
+display.newPolygon( display.contentHeight/2+350, display.contentWidth/2-292, verticiestPen)
+
+--Display Hexagon
+display.newPolygon(display.contentHeight/4+100, display.contentWidth/2, verticiesHex)
+
+--Display Heptagon
---
+++ MovingImagesAlexander/main.lua
@@ -0,0 +1,61 @@
+-----------------------------------------------------------------------------------------
+--Title: Moving Images
+--Name: Alexander
+--Course: ICS20/3C
+--This program moves a beetleship across the screen and then makes it fade out.
+--I added a second Image that shrinks in size and moves in the opposite direction, then fades out.
+-----------------------------------------------------------------------------------------
+
+--Hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--global variables
+scrollspeed=3
+scrollspeedRocket=7
+
+--Background image width and height
+local bacgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+
+--Character Image with width and height.
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+--Make image invisible
+beetleship.alpha = 0
+
+--set initial cordinates
+beetleship.x = 0
+beetleship.y = display.contentHeight/3
+
+--Function: Move Beetleship
+--Input: this function acceptes an event listener
+--Output: none
+--Description: This function adds the scroll speed to the x value of the ship.
+local function MoveShip(event)
+	--add the scrool speed to the x value of the ship
+	beetleship.x= beetleship.x + scrollspeed
+	--Change the transparency of the ship
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+--MoveShip will be called over and over
+Runtime:addEventListener("enterFrame", MoveShip)
+
+-- make new image
+local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
+
+--set coordinates
+rocketship.x = display.contentWidth
+rocketship.y = display.contentHeight
+
+--move rocket ship and shrink it as well as fade out
+local function MoveShip(event)
+	--subtract scrollspeedRocket to the x and y values
+	rocketship.x = rocketship.x - scrollspeedRocket
+	rocketship.y = rocketship.y - scrollspeedRocket
+	--fade out
+	rocketship.alpha = rocketship.alpha - 0.01
+
+end
+
+--MoveShip will be called over and over
+Runtime:addEventListener("enterFrame", MoveShip)
