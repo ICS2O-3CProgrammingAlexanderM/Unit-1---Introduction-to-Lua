d08d9f668e6392ecf07648147334048f93066960
Alexander Matheson <Alexander.Matheson@ocsbstudent.ca>
2020-03-12 14:27:33 -0400

    finished day 17

--- NumericTextFieldsAlexander/main.lua
+++ NumericTextFieldsAlexander/main.lua
@@ -16,6 +16,8 @@ display.setDefault("background", 50/255, 50/255, 255/255)
 -------------------------------------------------------------
 
 --Create variables
+local lives = 3
+local points = 0
 local questionObject
 local correctObject
 local numericField
@@ -24,6 +26,7 @@ local randomNumber2
 local userAnswer
 local correctAnswer
 local incorrectAnswer
+local Win
 
 -------------------------------------------------------------
 -- LOCAL FUNCTIONS
@@ -50,6 +53,26 @@ local function HideIncorrect()
 	AskQuestion()
 end
 
+local function ShowWin()
+	correctObject.isVisible = false
+	incorrectAnswer.isVisible = false
+	pointsText.isVisible = false
+	livesText.isVisible = false
+	questionObject.isVisible = false
+	numericField.isVisible = false
+	win.isVisible = true
+end
+
+local function ShowOver()
+	correctObject.isVisible = false
+	incorrectAnswer.isVisible = false
+	pointsText.isVisible = false
+	livesText.isVisible = false
+	questionObject.isVisible = false
+	numericField.isVisible = false
+	over.isVisible = true
+end
+
 local function NumericFieldListener( event )
 	--user editd "numeric field"
 	if(event.phase == "began") then
@@ -57,19 +80,37 @@ local function NumericFieldListener( event )
 		--clear field
 		event.target.text = ""
 
-	elseif (event.phase == "submited") then
+	elseif (event.phase == "submitted") then
 
 		--when the enter key is pressed set imput to answer
 		userAnswer = tonumber(event.target.text)
 
 		--if the answer is correct
 		if (userAnswer == correctAnswer) then
+			--Update points
+			points = points + 1
+
+			--update text
+			pointsText.text = "Points = ".. points
+
 			correctObject.isVisible = true
 			timer.performWithDelay(2500, HideCorrect)
+			if (points == 5) then
+				timer.performWithDelay(2500, ShowWin)
+			end
 
 		else
+			--update lives
+			lives = lives - 1
+
+			--update text
+			livesText.text = "Lives = " .. lives
+
 			incorrectAnswer.isVisible=true
 			timer.performWithDelay(2500, HideIncorrect)
+			if (lives == 0) then
+				timer.performWithDelay(2500, ShowOver)
+			end
 
 		end
 	end
@@ -78,6 +119,12 @@ end
 --------------------------------------------------------------
 --OBJECT CREATION
 --------------------------------------------------------------
+
+--Display amount of points as a text object
+pointsText = display.newText("Points = " .. points, display.contentWidth-150, display.contentHeight/8, nil, 50)
+
+--display lives as a text object
+livesText = display.newText("Lives = ".. lives, 150, display.contentHeight/8, nil, 50)
 
 --Displays a question and sets color
 questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 50)
@@ -99,6 +146,14 @@ numericField.inputType= "number"
 --add event listener for numeric field
 numericField:addEventListener("userInput", NumericFieldListener)
 
+--Create the winning message
+win = display.newText("You Win!!!!", display.contentWidth/2, display.contentHeight/2, nil, 50)
+win:setTextColor(1, 1, 1)
+win.isVisible = false
+
+over = display.newText("Game Over", display.contentWidth/2, display.contentHeight/2, nil, 50)
+over:setTextColor(178/255, 31/255, 0/255)
+over.isVisible = false
 -------------------------------------------------------------------------
 --FUNCTION CALLS
 -------------------------------------------------------------------------
